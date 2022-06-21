5189bc1304088e2b64ab832a317f437983ff677a
Lauri Eskola <lauri.eskola@acquia.com>
2022-06-21 14:29:01 +0300

    Build process for next-acms

--- starters/basic-starter/pages/api/revalidate.ts
+++ starters/basic-starter/pages/api/revalidate.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import revalidate from "../../../../packages/next-acms/revalidate";
+import { revalidate } from 'next-acms';
 
 // API route for on-demand revalidation.
 // See https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#on-demand-revalidation-beta.
