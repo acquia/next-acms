018f27614e557e24cc72bf384d0a711bf622647d
Lauri Eskola <lauri.eskola@acquia.com>
2022-07-05 14:23:41 +0300

    Update preview

--- starters/basic-starter/pages/api/preview.tsx
+++ starters/basic-starter/pages/api/preview.tsx
@@ -1,5 +1,7 @@
-import { DrupalPreview } from 'next-drupal';
+import { drupal } from "lib/drupal";
 
 // API route for handling preview mode.
 // See https://nextjs.org/docs/advanced-features/preview-mode#step-1-create-and-access-a-preview-api-route.
-export default DrupalPreview();
+export default async function handler(request, response) {
+  return await drupal.preview(request, response);
+}
