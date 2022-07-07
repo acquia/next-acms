8f3749ffa9aed378baac3c0df8a7c60ecfa3f479
Phéna Proxima <adam@phenaproxima.net>
2022-07-07 13:06:42 -0400

    Use baseUrl in Cypress config

--- starters/basic-starter/cypress/e2e/spec.cy.ts
+++ starters/basic-starter/cypress/e2e/spec.cy.ts
@@ -1,7 +1,7 @@
 describe('Basic Starter', () => {
   it('has a navigation menu', () => {
-    cy.visit('http://localhost:3000')
-
-    cy.get('header nav').should('exist.and.be.visible')
+    cy.visit('/')
+        .get('header nav')
+        .should('exist.and.be.visible')
   })
 })
