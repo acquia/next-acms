31980d1f2d67394600742f9ace3ea5460a4a9dd2
Phéna Proxima <adam@phenaproxima.net>
2022-07-07 14:12:42 -0400

    Make it purty

--- cypress/e2e/basic-starter.cy.ts
+++ cypress/e2e/basic-starter.cy.ts
@@ -1,7 +1,5 @@
 describe('Basic Starter', () => {
   it('has a navigation menu', () => {
-    cy.visit('/')
-        .get('header nav')
-        .should('exist.and.be.visible')
-  })
-})
+    cy.visit('/').get('header nav').should('exist.and.be.visible');
+  });
+});
