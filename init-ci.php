660df75ef8e9385694a8a016dfcb16cc48edd631
Phéna Proxima <adam@phenaproxima.net>
2022-07-11 15:58:17 -0400

    Delegate setup tasks to starter kit service

--- init-ci.php
+++ init-ci.php
@@ -9,47 +9,25 @@
  */
 
 use Drupal\consumers\Entity\Consumer;
-use Drupal\Core\File\FileSystemInterface;
-use Drupal\next\Entity\NextSite;
-use Drupal\user\Entity\User;
+
+/** @var \Drupal\acquia_cms_headless\Service\StarterkitNextjsService $starter_kit */
+$starter_kit = Drupal::service('acquia_cms_headless.starterkit_nextjs');
 
 // Create a user account to access Drupal data headlessly.
-/** @var \Drupal\user\UserInterface $account */
-$account = User::create()
-  ->enforceIsNew()
-  // Security is very important to us.
-  ->setPassword('Password')
-  ->setEmail('no-reply@example.com')
-  ->setUsername('Headless')
-  // We probably don't need any of this stuff.
-  ->set('langcode', 'en')
-  ->set('timezone', '')
-  ->set('init', 'no-reply@example.com')
-  ->set('preferred_langcode', 'en')
-  ->activate();
-$account->addRole('headless');
-$account->save();
+$starter_kit->createHeadlessUser();
+$accounts = Drupal::entityTypeManager()
+  ->getStorage('user')
+  ->loadByProperties(['name' => 'Headless']);
 
-// Generate OAuth keys.
-$dir = getcwd() . '/oauth_keys/sites/default';
-Drupal::service('file_system')->prepareDirectory($dir, FileSystemInterface::CREATE_DIRECTORY);
-Drupal::service('simple_oauth.key.generator')->generateKeys($dir);
-Drupal::configFactory()
-  ->getEditable('simple_oauth.settings')
-  ->set('public_key', "$dir/public.key")
-  ->set('private_key', "$dir/private.key")
-  ->save();
+if (empty($accounts)) {
+  throw new \Exception("The Headless user does not appear to exist.");
+}
+$account = reset($accounts);
+
+$starter_kit->generateOauthKeys();
 
 // Find the default consumer and make it not-the-default.
-/** @var \Drupal\consumers\Entity\Consumer[] $consumers */
-$consumers = Drupal::entityTypeManager()
-  ->getStorage('consumer')
-  ->loadByProperties(['label' => 'Default Consumer']);
-
-if (empty($consumers)) {
-  throw new \Exception("The default consumer doesn't exist, bruh.");
-}
-reset($consumers)->set('is_default', FALSE)->save();
+$starter_kit->updateDefaultConsumer(FALSE);
 
 // Create the headless consumer.
 $secret = Drupal::service('password_generator')->generate(21);
@@ -64,18 +42,8 @@ $consumer->set('roles', 'headless');
 $consumer->set('user_id', $account->id());
 $consumer->save();
 
-// Create the next.js site entity.
-$site = NextSite::create([
-  'id' => 'headless',
-  'label' => 'Headless Site 1',
-  'base_url' => 'http://localhost:3000/',
-  'preview_url' => 'http://localhost:3000/api/preview/',
-  'preview_secret' => Drupal::service('password_generator')->generate(21),
-]);
-$site->save();
-
-Drupal::service('acquia_cms_headless.starterkit_nextjs')
-  ->createHeadlessSiteEntities();
+$site = $starter_kit->createHeadlessSite();
+$starter_kit->createHeadlessSiteEntities();
 
 Drupal::configFactory()
   ->getEditable('acquia_cms_headless.settings')
@@ -96,4 +64,4 @@ $env = [
 foreach ($env as $key => $value) {
   $env[$key] = "$key=$value";
 }
-file_put_contents('.env.local', implode("\n", $env));
+echo implode("\n", $env);
