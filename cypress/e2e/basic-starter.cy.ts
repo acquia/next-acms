8a3814fbfe3ae10e8023b62b1498a8eb516d77ee
HARUMI JANG <harumi.jang@acquia.com>
2022-07-21 12:12:43 -0400

    added test for each taxonomy vocab

--- cypress/e2e/basic-starter.cy.ts
+++ cypress/e2e/basic-starter.cy.ts
@@ -22,15 +22,59 @@ describe('Basic Starter', () => {
 });
 
 describe('Taxonomy term page', () => {
-  it('should render articles for Design', () => {
+  it('should render articles for Blog (article type)', () => {
+    cy.visit('/taxonomy/term/1');
+    cy.get('h1').should('contain.text', 'Blog');
+    cy.get('article')
+      .should('have.length.greaterThan', 1)
+      .find('[data-cy="summary"]')
+      .should(($p) => {
+        expect($p.first()).to.contain.text('This is placeholder text.');
+      });
+  });
+
+  it('should render articles for Design (categories)', () => {
     cy.visit('/taxonomy/term/2');
     cy.get('h1').should('contain.text', 'Design');
     cy.get('article')
       .should('have.length.greaterThan', 1)
-      .find('p.summary')
+      .find('[data-cy="summary"]')
       .should(($p) => {
         expect($p.first()).to.contain.text('This is placeholder text.');
       });
+  });
+
+  it('should render people for Management (person type)', () => {
+    cy.visit('/taxonomy/term/3');
+    cy.get('h1').should('contain.text', 'Management');
+    cy.get('article')
+      .should('have.length.greaterThan', 1)
+      .find('.media__content')
+      .should('exist');
+    cy.get('article').find('h2').should('exist');
+  });
+
+  it('should render events for Webinar (event type)', () => {
+    cy.visit('/taxonomy/term/7');
+    cy.get('h1').should('contain.text', 'Webinar');
+    cy.get('article')
+      .should('have.length.greaterThan', 1)
+      .find('p')
+      .should(($p) => {
+        expect($p.first()).to.contain.text('This is placeholder text.');
+      });
+  });
+
+  it('should render places for Office (place type)', () => {
+    cy.visit('/taxonomy/term/13');
+    cy.get('h1').should('contain.text', 'Office');
+    cy.get('article')
+      .should('have.length.greaterThan', 1)
+      .find('h2')
+      .should(($h2) => {
+        expect($h2.first()).to.contain('Boston Head Office');
+      });
+    cy.get('article').find('.media__content').should('exist');
   });
 
   it('should still render for a taxonomy term with no nodes', () => {
