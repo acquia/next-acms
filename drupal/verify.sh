0a8baa211f55f9ccb871daddd64d529af9d18350
Phéna Proxima <adam@phenaproxima.net>
2022-08-17 12:02:25 -0400

    Container image should use SQLite by default

--- drupal/verify.sh
+++ drupal/verify.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
-set -e
-
+#
+# Starts the Acquia CMS site in an ephemeral container and confirms that it's
+# serving JSON:API data.
+#
 docker run --detach --publish 8080:80 --name drupal --rm phenaproxima/acquia_cms:headless
-docker cp settings.verify.php drupal:/opt/drupal/web/sites/default/settings.local.php
-docker exec --interactive --tty --workdir /opt/drupal/web drupal php ./core/scripts/db-tools.php import ../database.php.gz
+sleep 2
 curl --fail --silent http://127.0.0.1:8080/jsonapi
 docker stop drupal
