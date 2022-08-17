ac8e35fe94dcb414817c092642cd3c744e0ee657
Phéna Proxima <adam@phenaproxima.net>
2022-08-17 14:02:55 -0400

    Overwrite database in settings.ci.php

--- drupal/settings.ci.php
+++ drupal/settings.ci.php
@@ -3,11 +3,13 @@
 // These settings are copied to `settings.local.php` in the Acquia CMS
 // container used in our CI workflow.
 
-$database = &$databases['default']['default'];
 // This information needs to match the `database` service defined in our CI
 // configuration. See `.github/workflows/ci.yml`.
-$database['host'] = 'database';
-$database['user'] = 'drupal';
-$database['password'] = 'drupal';
-$database['database'] = 'drupal';
-$database['port'] = 3306;
+$databases['default']['default'] = [
+  'driver' => 'mysql',
+  'host' => 'database',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'database' => 'drupal',
+  'port' => 3306,
+];
