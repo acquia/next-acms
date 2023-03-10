e0bbf81b4b8e452ebcf757c63d1b754b30f04dc4
harumijang <35243020+harumijang@users.noreply.github.com>
2023-03-10 08:23:06 -0500

    Update dependencies (#227)
    Co-authored-by: Lauri Eskola <lauri.eskola@acquia.com>

--- examples/example-webform/pages/index.tsx
+++ examples/example-webform/pages/index.tsx
@@ -86,7 +86,7 @@ export default function WebformExample({ webform, id }) {
             ),
             button: withCustomStyles(WebformButton, {
               className: classNames(
-                'mt-3 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-2 border border-gray-400 rounded shadow',
+                'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow',
               ),
             }),
             email: withCustomStyles(
